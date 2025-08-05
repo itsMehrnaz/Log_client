3ce741424b2cd0ad56e38918694a6faf60a76e1e
Mehrnaz <mostlymarry04@gmail.com>
2025-08-05 20:32:08 +0330

    bindcode

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,54 @@
 mod packet;
+use std::io::stdin;
+use std::io::Write;
 use packet::LogPacket;
+use serde::{Serialize, Deserialize};
+use std::fs::write;
+use std::fs::OpenOptions;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
 
-fn main(){
-    let packet = LogPacket{
-        level:"info".to_string(),
-        message: "hello".to_string(),
-        timestamp: "12:34".to_string()
-    };
+fn write_on_file(log: LogPacket) -> std::io::Result<()> {
+    let encoded = bincode::serialize(&log).unwrap();
 
+    let mut file = OpenOptions::new()
+    .append(true)
+    .create(true)
+    .open("logs.bin")?;
 
-    let encoded: Vec<u8> = bincode::serialize(&packet).unwrap();
-    println!("encode is:{:?}", encoded);
+    file.write_all(&encoded)?;
+    file.write_all(b"/n")?;
 
-    let decoded : LogPacket = bincode::deserialize(&encoded).unwrap();
-    println!("decode is: {:?}", decoded);
+    Ok(())
 
+}
+
+fn read_logs() -> std::io::Result<Vec<LogPacket>>{
+    let file = File::open("logs.bin")?;
+    let reader = BufReader::new(file);
+
+    let mut logs = Vec::new();
+
+    for line in reader.lines() {
+        let line = line?;
+        let bytes = line.as_bytes();
+        if let Ok(log) = bincode::deserialize::<LogPacket>(bytes){
+            logs.push(log);
+        }
+    }
+
+    Ok(logs)
+}
+
+fn main() -> std::io::Result<()> {
+    let log = LogPacket::new("Info", "System started", "2025-07-31");
+
+    write_on_file(log)?;
+
+    let logs = read_logs()?;
+    for l in logs {
+        println!("{:?}", l);
+    }
+
+    Ok(())
 }
--- src/packet.rs
+++ src/packet.rs
@@ -6,3 +6,13 @@ pub struct LogPacket {
     pub message: String,
     pub timestamp: String
 }
+
+impl LogPacket{
+    pub fn new(level:&str, message: &str, timestamp:&str) -> Self{
+        Self { 
+               level: "Info".to_string(),
+               message: message.to_string(), 
+               timestamp: timestamp.to_string() 
+            }
+    }
+}
