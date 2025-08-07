823795e84078c514e462bd82d5dfe19e5c7486d2
Mehrnaz <mostlymarry04@gmail.com>
2025-08-07 11:34:00 +0330

    bincode

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,6 @@
 mod packet;
-use std::io::stdin;
-use std::io::Write;
-use packet::LogPacket;
-use serde::{Serialize, Deserialize};
-use std::fs::write;
-use std::fs::OpenOptions;
-use std::fs::File;
-use std::io::{BufRead, BufReader};
+use packet::Note;
+std::io::{BufReader, BufRead};
+use std::fs::{write, File, OpenOptions};
+use bincode::{serialize_into, deserialize_from};
 
-fn write_on_file(log: LogPacket) -> std::io::Result<()> {
-    let encoded = bincode::serialize(&log).unwrap();
-
-    let mut file = OpenOptions::new()
-    .append(true)
-    .create(true)
-    .open("logs.bin")?;
-
-    file.write_all(&encoded)?;
-    file.write_all(b"/n")?;
-
-    Ok(())
-
-}
-
-fn read_logs() -> std::io::Result<Vec<LogPacket>>{
-    let file = File::open("logs.bin")?;
-    let reader = BufReader::new(file);
-
-    let mut logs = Vec::new();
-
-    for line in reader.lines() {
-        let line = line?;
-        let bytes = line.as_bytes();
-        if let Ok(log) = bincode::deserialize::<LogPacket>(bytes){
-            logs.push(log);
-        }
-    }
-
-    Ok(logs)
-}
-
-fn main() -> std::io::Result<()> {
-    let log = LogPacket::new("Info", "System started", "2025-07-31");
-
-    write_on_file(log)?;
-
-    let logs = read_logs()?;
-    for l in logs {
-        println!("{:?}", l);
-    }
-
-    Ok(())
-}
--- src/packet.rs
+++ src/packet.rs
@@ -1,18 +1,15 @@
 use serde::{Serialize, Deserialize};
 
 #[derive(Debug, Serialize, Deserialize)]
-pub struct LogPacket {
-    pub level: String,
-    pub message: String,
-    pub timestamp: String
+pub struct Note {
+    pub title: String,
+    pub body: String
 }
 
-impl LogPacket{
-    pub fn new(level:&str, message: &str, timestamp:&str) -> Self{
-        Self { 
-               level: "Info".to_string(),
-               message: message.to_string(), 
-               timestamp: timestamp.to_string() 
+impl Note  {
+    fn new(title: &str, body: &str) -> Self{
+        Self { title: title.to_string(),
+               body: body.to_string() 
             }
     }
 }
