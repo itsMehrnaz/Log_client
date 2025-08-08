6255f394ebe579932a687399a5a44290a5e23677
Mehrnaz <mostlymarry04@gmail.com>
2025-08-09 01:11:42 +0330

    upgrade bincode

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,30 @@
 mod packet;
 use packet::Note;
+use serde::Deserialize;
 use std::io::{BufReader, BufWriter};
 use std::fs::{write, File, OpenOptions};
 use bincode::{serialize_into, deserialize_from};
 
 fn main() -> std::io::Result<()>{
     let logs = vec![
-        Note::new("title", "body")
+        Note::new("title", "body"),
+        Note::new("title", "body"),
+        Note::new("title", "body"),
+        Note::new("title", "body"),
     ];
 
     let file = File::create("log.bin")?;
-    let writer = BufWriter::new(file);
+    let mut writer = BufWriter::new(file);
+    serialize_into(&mut writer, &logs).expect("error to serilize");
+
+    let file = File::open("log.bin")?;
+    let mut reader = BufReader::new(file);
+    let deserialize_code :Vec<Note> = deserialize_from(&mut reader).expect("error to deserilize");
+
+    println!("error");
+    for log in deserialize_code.iter().filter(|l| l.title == "hi") {
+        println!("{:?}", log);
+    }
+
+    Ok(())
 }
